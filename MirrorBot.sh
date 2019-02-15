3a4370839c82a5f8aff24c750959ce226a1997a1
acoffeerunner <acoffeerunner@gmail.com>
2019-02-15 09:32:26 +0000

    main: shift all the download workflow to another folder This makes pushing to git easier for me cuz then, I don't have to keep looking in this directory for any of the test files I was downloading.

--- MirrorBot.sh
+++ MirrorBot.sh
@@ -1,5 +1,7 @@
 echo "Enter URL to be uploaded to Google Drive: "
 read link
-wget --content-disposition --max-redirect 100000 $link -O "file"
+wget --content-disposition --max-redirect 100000 $link -O "/home/acoffeerunner/Downloads/file"
+cd ~/Downloads
 mv file $(basename $link)
 gdrive upload $(basename $link)
+cd ../MirrorBot
